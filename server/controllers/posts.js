64b4dba453408ee59551b751b2935987419c9515
antonyjes <72809149@continental.edu.pe>
2023-02-16 22:05:14 -0500

    Get Post - Server config

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -43,6 +43,16 @@ export const getUserPosts = async (req, res) => {
     }
 }
 
+export const getPost = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const post = await Post.findById(id);
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
 /* UPDATE */
 export const likePost = async (req, res) => {
     try {
